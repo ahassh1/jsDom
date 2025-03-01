208686233d6841ff4057c3279df136eaf02aa6b2
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-03-01 16:46:54 +0600

    addEvenListener part two

---
+++ js/handler.js
@@ -0,0 +1,9 @@
+console.log('handler file added');
+
+document.getElementById('btn-update-title')
+.addEventListener('click', function () {
+    // console.log('button clicked')
+    const pageTitleElement = document.getElementById('page-title');
+    console.log(pageTitleElement);
+    pageTitleElement.innerText = "updated page title text ";
+}) 
